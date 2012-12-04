5e81d347dd4e25557a420d35c6e7d32790ace692
aommiez <aommiez@gmail.com>
2012-12-04 08:59:56 +0000

    11

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-        Yii::app()->facebook->initJs();
+        //Yii::app()->facebook->initJs();
         if ( !empty($_GET['p'])) {
             $p = $_GET['p'];
             $this->render('show',array( 'p' => $p));
