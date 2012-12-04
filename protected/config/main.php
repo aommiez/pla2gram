fd7a1263eb9ff8ead038b6801cda17f73d474145
aommiez <aommiez@gmail.com>
2012-12-04 08:52:56 +0000

    11

--- protected/config/main.php
+++ protected/config/main.php
@@ -61,7 +61,7 @@ return array(
             //'fileUpload'=>false, // needed to support API POST requests which send files
             //'trustForwarded'=>false, // trust HTTP_X_FORWARDED_* headers ?
             'locale'=>'en_US', // override locale setting (defaults to en_US)
-            //'jsSdk'=>true, // don't include JS SDK
+            'jsSdk'=>true, // don't include JS SDK
             'async'=>true, // load JS SDK asynchronously
             //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
             //'status'=>true, // JS SDK - check login status
@@ -69,7 +69,7 @@ return array(
             'oauth'=>true,  // JS SDK - enable OAuth 2.0
             'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
             //'frictionlessRequests'=>true, // JS SDK - enable frictionless requests for request dialogs
-            //'html5'=>true,  // use html5 Social Plugins instead of XFBML
+            'html5'=>true,  // use html5 Social Plugins instead of XFBML
             //'ogTags'=>array(  // set default OG tags
             //'title'=>'MY_WEBSITE_NAME',
             //'description'=>'MY_WEBSITE_DESCRIPTION',
