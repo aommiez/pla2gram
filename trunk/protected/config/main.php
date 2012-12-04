e18c96d3b336b4d8879de74a9383041e30ddd9d1
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-04 08:56:56 +0000

    11

--- trunk/protected/config/main.php
+++ trunk/protected/config/main.php
@@ -64,7 +64,7 @@ return array(
             'jsSdk'=>true, // don't include JS SDK
             'async'=>true, // load JS SDK asynchronously
             //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
-            //'status'=>true, // JS SDK - check login status
+            'status'=>true, // JS SDK - check login status
             'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
             'oauth'=>true,  // JS SDK - enable OAuth 2.0
             'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
