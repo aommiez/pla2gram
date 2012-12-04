38626973ea999d8dedea7c2c791c0177145a56c1
aommiez <aommiez@gmail.com>
2012-12-04 09:25:36 +0000

    11

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -27,8 +27,24 @@ Helper::register('jquery-1.8.3.min.js');
     <div id="logo"><a href="<?php echo "/";?>" style="color: #ffffff;text-decoration: none;">PLA2GRAM.COM</a></div>
 
 	<?php
-        echo Yii::app()->facebook->getUser();
-        //echo $content;
+
+
+        if ( Yii::app()->facebook->getUser() == 0 ) {
+            $params = array(
+                'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions , user_online_presence, publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
+                'redirect_uri' => 'https://www.pla2gram.com/',
+                'display' => 'popup'
+            );
+            $fbUrl = Yii::app()->facebook->getLoginUrl($params);
+            echo <<<HTML
+            <a href="{$fbUrl}">
+              <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+            </a>
+HTML;
+
+        } else {
+            echo $content;
+        }
     ?>
     <div>
         <div id="last_upload">Last Upload</div>
