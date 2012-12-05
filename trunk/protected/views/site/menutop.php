0f8c7f459057a61b853ae2f9eec2d1af9e228576
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-05 04:58:48 +0000

    1

--- trunk/protected/views/site/menutop.php
+++ trunk/protected/views/site/menutop.php
@@ -7,3 +7,57 @@
  * Email : aommiez@gmail.com
  * File Name : menutop.php
  */
+/*
+if ( Yii::app()->facebook->getUser() == 0 ) {
+    $params = array(
+        'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions , user_online_presence, publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
+        'redirect_uri' => 'http://www.pla2gram.com/'
+    );
+    $fbUrl = Yii::app()->facebook->getLoginUrl($params);
+    echo <<<HTML
+            <a href="{$fbUrl}">
+              <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+            </a>
+HTML;
+} else {
+    $fbID = Yii::app()->facebook->getUser();
+    $apiFbKey = md5(trim("Yii::app()->facebook->api($fbID)"));
+    if ( Yii::app()->cache->get($apiFbKey) == false ) {
+        $results = Yii::app()->facebook->api($fbID);
+        Yii::app()->cache->set($apiFbKey,$results,1800);
+        $fbInfo = $results;
+    } else {
+        $fbInfo = Yii::app()->cache->get($apiFbKey);
+    }
+    $params = array( 'next' => 'http://www.pla2gram.com/' );
+    $fbUrl = Yii::app()->facebook->getLogoutUrl($params);
+    $fbNickname = $fbInfo['name'];
+    echo <<<HTML
+<div id="userZone">
+    <div id="fbImg">
+        <img src="https://graph.facebook.com/{$fbID}/picture"/>
+    </div>
+    <div id="fbNickname">
+        {$fbNickname}
+    </div>
+    <div>
+        <a href="{$fbUrl}">Log Out !</a>
+    </div>
+</div>
+HTML;
+}
+*/
+if ( Yii::app()->facebook->getUser() == 0 ) {
+    $params = array(
+        'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions , user_online_presence, publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
+        'redirect_uri' => 'http://www.pla2gram.com/'
+    );
+    $fbUrl = Yii::app()->facebook->getLoginUrl($params);
+    echo <<<HTML
+            <a href="{$fbUrl}">
+              <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+            </a>
+HTML;
+} else {
+    echo "Login with Facebook is ok !";
+}
