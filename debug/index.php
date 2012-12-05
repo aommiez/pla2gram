b975c75e6733ae9f862546f8ca29ebfdb1cdf4d1
aommiez <aommiez@gmail.com>
2012-12-05 07:04:38 +0000

    111

---
+++ debug/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aOmMiez ( Mir4ge )
+ * Date: 12/5/12 AD
+ * Time: 1:58 PM
+ * Email : aommiez@gmail.com
+ * File Name : index.php
+ */
+
+$im = new Imagick("test.JPG");
