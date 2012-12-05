123e7276b0c358073d22bbf6e16a66736b649839
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-05 05:36:31 +0000

    111

--- trunk/protected/views/site/menutop.php
+++ trunk/protected/views/site/menutop.php
@@ -63,7 +63,7 @@ HTML;
     //$fbInfo = GetController::getFbUser();
     $params = array( 'next' => 'http://www.pla2gram.com/' );
     $fbUrl = Yii::app()->facebook->getLogoutUrl($params);
-    $fbNickname = $fbInfo['name'];
+    //$fbNickname = $fbInfo['name'];
     $fbID = Yii::app()->facebook->getUser();
     echo <<<HTML
 <div id="userZone">
