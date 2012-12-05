6585d3a8c37037e4b746cb133f5ce89a040757ee
aommiez <aommiez@gmail.com>
2012-12-05 06:21:45 +0000

    111

--- protected/views/site/show.php
+++ protected/views/site/show.php
@@ -17,7 +17,7 @@ $image=urlencode("http://www.pla2gram.com/thumb/thumb_".$thumb);
 
 <?php
 echo <<<HTML
-    <div>
+    <div style="text-align: center;color: white;text-align: left;">
         <div id="showPhotoDiv">
             <div id="photo"><img src="/{$link}" id="photo_img"></div>
         </div>
@@ -42,7 +42,7 @@ HTML;
         text-align: center;
     }
     #photo {
-        margin-top: 16px;
+
     }
     #timeCreate {
         margin-top: 10px;
@@ -56,24 +56,32 @@ HTML;
     a{
         text-decoration: none;
     }
-#btn_fbshare {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 14px;
-    color: #3B5998;
-    padding: 10px 8px;
-    background: -moz-linear-gradient( top, white 0%, white);
-    background: -webkit-gradient( linear, left top, left bottom, from(white), to(white));
-    -moz-border-radius: 5px;
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    border: 0px solid black;
-    -moz-box-shadow: 0px 1px 3px rgba(000,000,000,0.5), inset 0px 0px 0px rgba(076,189,255,0);
-    -webkit-box-shadow: 0px 1px 3px rgba(000, 000, 000, 0.5), inset 0px 0px 0px rgba(076, 189, 255, 0);
-    box-shadow: 0px 1px 3px rgba(000, 000, 000, 0.5), inset 0px 0px 0px rgba(076, 189, 255, 0);
-    text-shadow: 0px -1px 3px rgba(255, 255, 255, 0.8), 0px 1px 0px rgba(255, 255, 255, 0.3);
-    line-height: 29px;
-    vertical-align: bottom;
-    font-size: 20px;
-}
-
+    #btn_fbshare {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 14px;
+        color: #3B5998;
+        padding: 10px 8px;
+        background: -moz-linear-gradient( top, white 0%, white);
+        background: -webkit-gradient( linear, left top, left bottom, from(white), to(white));
+        -moz-border-radius: 5px;
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+        border: 0px solid black;
+        -moz-box-shadow: 0px 1px 3px rgba(000,000,000,0.5), inset 0px 0px 0px rgba(076,189,255,0);
+        -webkit-box-shadow: 0px 1px 3px rgba(000, 000, 000, 0.5), inset 0px 0px 0px rgba(076, 189, 255, 0);
+        box-shadow: 0px 1px 3px rgba(000, 000, 000, 0.5), inset 0px 0px 0px rgba(076, 189, 255, 0);
+        text-shadow: 0px -1px 3px rgba(255, 255, 255, 0.8), 0px 1px 0px rgba(255, 255, 255, 0.3);
+        line-height: 29px;
+        vertical-align: bottom;
+        font-size: 20px;
+    }
+    #showPhotoDiv {
+        display: inline-block;
+        width: 550px;
+    }
+    #showDetail {
+        display: inline-block;
+        width: 300px;
+        vertical-align: top;
+    }
 </style>
