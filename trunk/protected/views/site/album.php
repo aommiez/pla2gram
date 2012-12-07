f4b381486d1ee165aff0df1c1e58bd4d2133f392
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-07 07:38:26 +0000

    1111

--- trunk/protected/views/site/album.php
+++ trunk/protected/views/site/album.php
@@ -9,8 +9,7 @@
  */
 Helper::YiiImport("GetController");
 $albums = GetController::getAlbums(Yii::app()->facebook->getUser());
+echo "<pre>";
+print_r($albums);
+echo "</pre>";
 
-
-foreach ( $albums as $key => $value ) {
-    echo $value['name']."<br>";
-}
