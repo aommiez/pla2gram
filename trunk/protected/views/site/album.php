67d33dceb1387f815cbd369293a44d82d38990ed
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-07 09:00:13 +0000

    1111

--- trunk/protected/views/site/album.php
+++ trunk/protected/views/site/album.php
@@ -10,20 +10,14 @@
 Helper::YiiImport("GetController");
 
 $albums = GetController::getAlbums();
-/*
+
 foreach($albums['data'] as $album)
 {
     $albumID = $album['id'];
     $albumName = $album['name'];
 }
-*/
-for($i = 0;$i<count($albums['data']);$i++){
-    for($j = 0;$j<count($albums['data'][$i]);$j++){
-        if($albums['data'][$i]['name'] == "Cover Photos"){
-            echo Yii::app()->facebook->api('/me/albums/'.$albums['data'][$i]['cover_photo'],'GET');
-        }
-    }
-}
+
+
 /*
 echo "<pre>";
 print_r($albums);
