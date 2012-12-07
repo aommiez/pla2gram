03d77c7041bda277e8d8ecfcfcd30a9a0f08232b
aommiez <aommiez@gmail.com>
2012-12-07 11:12:21 +0000

    1111

--- protected/views/site/lastUpload.php
+++ protected/views/site/lastUpload.php
@@ -32,7 +32,7 @@ Helper::register("dimsemenov-preview-pack.css");
             echo <<<HTML
             <li class="touchcarousel-item">
             <a class="item-block" href="/?p={$i}">
-                <img src="{$l}"  height="230" />
+                <img src="{$l}" />
 
             </a>
         </li>
