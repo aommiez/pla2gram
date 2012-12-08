939353c955b674fa1ecea45c7a8694675227c41b
aommiez <aommiez@gmail.com>
2012-12-08 07:42:41 +0000

    1111111

--- protected/views/site/album.php
+++ protected/views/site/album.php
@@ -37,7 +37,7 @@ $albums = GetController::getAlbums();
                         var subImages_text1 = "Photo "+(v+1);
 
                         //this is for the small picture that comes in the second column
-                        var subImages_text2 = '<img src="'+image_arr[3]["source"]+'" class="fbPhoto"/> ';
+                        var subImages_text2 = '<img src="'+image_arr[6]["source"]+'" class="fbPhoto"/> ';
 
                         //this is for the third column, which holds the links other size versions of a picture
                         var subImages_text3 = "";
@@ -56,7 +56,7 @@ $albums = GetController::getAlbums();
             });
 
             function addNewRow(subImages_text1,subImages_text2,subImages_text3,paging ) {
-                $("#photoAlbum").append(subImages_text3);
+                $("#photoAlbum").append(subImages_text2);
             }
     });
 </script>
