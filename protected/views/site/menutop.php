03b5b0f468878c5338b1118eb0a1eb3590033867
aommiez <aommiez@gmail.com>
2012-12-08 08:17:46 +0000

    1111111

--- protected/views/site/menutop.php
+++ protected/views/site/menutop.php
@@ -57,43 +57,25 @@ HTML;
     }
 }
 
-
-
-/*
-
-if ( Yii::app()->facebook->getUser() == 0 ) {
-    $params = array(
-        'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions , user_online_presence, publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
-        'redirect_uri' => 'http://www.pla2gram.com/'
-    );
-    $fbUrl = Yii::app()->facebook->getLoginUrl($params);
-    echo <<<HTML
-    <div id="facebook-login-btb">
-		<a href="{$fbUrl}">login with <span>facebook</span></a>
-	</div>
-HTML;
-} else {
-    Helper::YiiImport("GetController");
-    $fbInfo = GetController::getFbUser();
-
-    //$params = array( 'next' => 'http://www.pla2gram.com/' );
-    //$fbUrl = Yii::app()->facebook->getLogoutUrl($params);
-    $fbNickname = $fbInfo['name'];
-    $fbID = Yii::app()->facebook->getUser();
-    $albumLink = Yii::app()->createUrl("site/album");
-    echo <<<HTML
-<div id="userZone">
-    <div id="fbImg">
-        <img src="https://graph.facebook.com/{$fbID}/picture"/>
-    </div>
-    <div id="fbNickname">
-        {$fbNickname}
-    </div>
-    <div id="userMenu">
-        <span><a href="{$albumLink}">Albums</a></span>
-    </div>
-</div>
-HTML;
-}*/
-
 ?>
+<script>
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+            alert(accessToken);
+        } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook,
+            // but has not authenticated your app
+            alert("not_authorized");
+        } else {
+            // the user isn't logged in to Facebook.
+            alert("notlogin");
+        }
+    });
+</script>
