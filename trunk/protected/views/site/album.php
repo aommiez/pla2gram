81690649072eb8a722b54e7f978ed3c460dcd84f
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-08 05:58:46 +0000

    1111111

--- trunk/protected/views/site/album.php
+++ trunk/protected/views/site/album.php
@@ -12,6 +12,11 @@ Helper::YiiImport("GetController");
 $albums = GetController::getAlbums();
 
 ?>
+<style>
+    .fbPhoto {
+
+    }
+</style>
 <script>
     jQuery(function($) {
             $("#albumList").change( function() {
@@ -20,14 +25,14 @@ $albums = GetController::getAlbums();
                 var albumID = $(this).val();
                 FB.api("/"+albumID+"/photos",function(response){
                     var photos = response["data"];
-                    document.getElementById("photoCount").innerHTML = "Photos("+photos.length+")";
+                    //document.getElementById("photoCount").innerHTML = "Photos("+photos.length+")";
                     for(var v=0;v<photos.length;v++) {
                         var image_arr = photos[v]["images"];
 
                         var subImages_text1 = "Photo "+(v+1);
 
                         //this is for the small picture that comes in the second column
-                        var subImages_text2 = '<img src="'+image_arr[(image_arr.length-1)]["source"]+'" />';
+                        var subImages_text2 = '<img src="'+image_arr[(image_arr.length-1)]["source"]+'" class="fbPhoto"/>';
 
                         //this is for the third column, which holds the links other size versions of a picture
                         var subImages_text3 = "";
@@ -42,7 +47,7 @@ $albums = GetController::getAlbums();
             });
 
             function addNewRow(subImages_text1,subImages_text2,subImages_text3) {
-                $("#photoAlbum").append(subImages_text3);
+                $("#photoAlbum").append(subImages_text2);
             }
     });
 </script>
