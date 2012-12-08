04e40c52674783500c4dfe5c1483139b3569a468
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-08 04:12:03 +0000

    1111

--- trunk/protected/controllers/GetController.php
+++ trunk/protected/controllers/GetController.php
@@ -78,7 +78,7 @@ class GetController extends Controller
     }
     public static function getAlbums (){
         if ( Yii::app()->facebook->getUser() == 0 ) {
-            GetController::FbLogin(Yii::app()->request->requestUri);
+            GetController::FbLogin('http://www.pla2gram.com/'.Yii::app()->request->requestUri);
         } else {
             $access = Yii::app()->facebook->getAccessToken();
             $albums = Yii::app()->facebook->api('/'.Yii::app()->facebook->getUser().'/albums?access_token='.$access);
