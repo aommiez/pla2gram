17d9c2c9468a6abfade12693551ac6a5680d66bc
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-08 04:32:27 +0000

    1111

--- trunk/protected/views/site/album.php
+++ trunk/protected/views/site/album.php
@@ -16,7 +16,27 @@ $albums = GetController::getAlbums();
     jQuery(function($) {
             $("#albumList").change( function() {
                 var albumID = $(this).val();
-                alert(albumID);
+                FB.api("/"+albumID+"/photos",function(response){
+                    var photos = response["data"];
+                    document.getElementById("photos_header").innerHTML = "Photos("+photos.length+")";
+                    for(var v=0;v<photos.length;v++) {
+                        var image_arr = photos[v]["images"];
+
+                        var subImages_text1 = "Photo "+(v+1);
+
+                        //this is for the small picture that comes in the second column
+                        var subImages_text2 = '<img src="'+image_arr[(image_arr.length-1)]["source"]+'" />';
+
+                        //this is for the third column, which holds the links other size versions of a picture
+                        var subImages_text3 = "";
+
+                        //gets all the different sizes available for a given image
+                        for(var j = 0 ;j<image_arr.length;j++) {
+                            subImages_text3 += '<a target="_blank" href="'+image_arr[j]["source"]+'">Photo('+image_arr[j]["width"]+"X"+image_arr[j]["height"]+')</a><br/>';
+                        }
+                        addNewRow(subImages_text1,subImages_text2,subImages_text3);
+                    }
+                });
             });
     });
 </script>
@@ -33,6 +53,6 @@ HTML;
     }
     ?>
 </select>
-<div>
+<div id="photos_header">
 
 </div>
