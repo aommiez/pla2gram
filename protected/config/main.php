2fcf25e014e1ce127ae4f6d4d20c38a123e2fbe0
aommiez <aommiez@gmail.com>
2012-12-11 05:04:04 +0000

    1111111

--- protected/config/main.php
+++ protected/config/main.php
@@ -65,7 +65,7 @@ return array(
             //'async'=>true, // load JS SDK asynchronously
             //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
             //'status'=>true, // JS SDK - check login status
-            'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
+            //'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
             'oauth'=>true,  // JS SDK - enable OAuth 2.0
             'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
             //'frictionlessRequests'=>true, // JS SDK - enable frictionless requests for request dialogs
