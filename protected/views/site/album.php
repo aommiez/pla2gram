7cd9f37a3eb54facc24aae03683f6c4081fe503d
aommiez <aommiez@gmail.com>
2012-12-11 06:18:23 +0000

    1111111

--- protected/views/site/album.php
+++ protected/views/site/album.php
@@ -10,6 +10,8 @@
 Helper::YiiImport("GetController");
 
 $albums = GetController::getAlbums();
+$access = Yii::app()->facebook->getAccessToken();
+$albumSyncKey = md5(trim('/'.Yii::app()->facebook->getUser().'/albums?access_token='.$access));
 print_r(Yii::app()->cache->get($albumSyncKey));
 ?>
 <style>
