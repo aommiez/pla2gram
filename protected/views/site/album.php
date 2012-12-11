739e53188f660bbbcd34009b89552c573f64144d
aommiez <aommiez@gmail.com>
2012-12-11 05:45:29 +0000

    1111111

--- protected/views/site/album.php
+++ protected/views/site/album.php
@@ -12,7 +12,7 @@ $user_id = Yii::app()->facebook->getUser();
 if (!$user_id) {
     $params = array(
         'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions ,  publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
-        'redirect_uri' => 'http://www.pla2gram.com/'
+        'redirect_uri' => 'http://www.pla2gram.com/'.Yii::app()->request->requestUri
     );
     $fbUrl = Yii::app()->facebook->getLoginUrl($params);
     echo <<<HTML
