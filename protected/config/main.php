0b6dcad4ad14a5d3f09b06b60c4af14fd4080392
aommiez <aommiez@gmail.com>
2012-12-11 05:17:25 +0000

    1111111

--- protected/config/main.php
+++ protected/config/main.php
@@ -64,7 +64,7 @@ return array(
             'locale'=>'en_US', // override locale setting (defaults to en_US)
             'jsSdk'=>true, // don't include JS SDK
             'async'=>true, // load JS SDK asynchronously
-            'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
+            //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
             'status'=>true, // JS SDK - check login status
             'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
             'oauth'=>true,  // JS SDK - enable OAuth 2.0
