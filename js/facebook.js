bea57cc93c84d3c1f02bc139463d7358174ec342
aommiez <aommiez@gmail.com>
2012-12-11 04:46:38 +0000

    1111111

--- js/facebook.js
+++ js/facebook.js
@@ -1,19 +1,5 @@
-FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-        // the user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, a signed
-        // request, and the time the access token
-        // and signed request each expire
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-        alert(accessToken);
-    } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook,
-        // but has not authenticated your app
-        alert("not_authorized");
-    } else {
-        alert("not_login");
-        // the user isn't logged in to Facebook.
-    }
+$(document).ready(function(){
+    FB.Event.subscribe('auth.login', function(){
+        alert('logged in!');
+    });
 });
