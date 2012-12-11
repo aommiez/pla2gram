6fd969f458b66c4a8375cdf110bcd60dc610f283
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-11 05:44:04 +0000

    1111111

--- trunk/protected/views/site/album.php
+++ trunk/protected/views/site/album.php
@@ -8,7 +8,19 @@
  * File Name : album.php
  */
 Helper::YiiImport("GetController");
+if (!$user_id) {
+    $params = array(
+        'scope' => 'email ,user_about_me, user_activities, user_likes, user_location ,user_photos, user_status, user_videos, friends_about_me, friends_likes, friends_photos, publish_actions ,  publish_stream, offline_access , status_update , photo_upload , video_upload , publish_checkins',
+        'redirect_uri' => 'http://www.pla2gram.com/'
+    );
+    $fbUrl = Yii::app()->facebook->getLoginUrl($params);
+    echo <<<HTML
+    <div id="facebook-login-btb">
+		<a href="{$fbUrl}">login with <span>facebook</span></a>
+	</div>
+HTML;
 
+}
 $albums = GetController::getAlbums();
 
 ?>
