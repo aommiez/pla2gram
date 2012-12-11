e707d2238eb2394fc2712055a18781f351d81eae
aommiez <aommiez@gmail.com>
2012-12-11 05:06:27 +0000

    1111111

--- protected/config/main.php
+++ protected/config/main.php
@@ -54,28 +54,7 @@ return array(
 
             ),
         ),
-        'facebook'=>array(
-            'class' => 'ext.yii-facebook-opengraph.SFacebook',
-            'appId'=>'413892292015301', // needed for JS SDK, Social Plugins and PHP SDK
-            'secret'=>'5fa170f45bec31af24abbdcb291b0dab', // needed for the PHP SDK
-            'fileUpload'=>true, // needed to support API POST requests which send files
-            //'trustForwarded'=>false, // trust HTTP_X_FORWARDED_* headers ?
-            'locale'=>'en_US', // override locale setting (defaults to en_US)
-            //'jsSdk'=>true, // don't include JS SDK
-            //'async'=>true, // load JS SDK asynchronously
-            //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
-            //'status'=>true, // JS SDK - check login status
-            //'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
-            //'oauth'=>true,  // JS SDK - enable OAuth 2.0
-            //'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
-            //'frictionlessRequests'=>true, // JS SDK - enable frictionless requests for request dialogs
-            //'html5'=>true,  // use html5 Social Plugins instead of XFBML
-            //'ogTags'=>array(  // set default OG tags
-            //'title'=>'PLA2GRAM.COM : Stylize your photo',
-            //'description'=>'PLA2GRAM.COM : Stylize your photo',
-            //'image'=>'URL_TO_WEBSITE_LOGO',
-            //),
-        ),
+
         'cache'=>array(
             'class'=>'system.caching.CMemCache',
             'servers'=>array(
