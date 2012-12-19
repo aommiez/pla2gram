3818f7c4a984bb4a53d12edb944f81646f723991
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-19 10:04:10 +0000

    1010

--- trunk/protected/views/layouts/main.php
+++ trunk/protected/views/layouts/main.php
@@ -31,23 +31,7 @@ if ( isset($_GET['code'])) {
 
 </head>
 <body>
-<script type="text/javascript">
-    function hideAddressBar()
-    {
-        if(!window.location.hash)
-        {
-            if(document.height < window.outerHeight)
-            {
-                document.body.style.height = (window.outerHeight + 50) + 'px';
-            }
 
-            setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-        }
-    }
-
-    window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-    window.addEventListener("orientationchange", hideAddressBar );
-</script>
 
 <div class="container" id="page">
 
