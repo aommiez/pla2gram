a4edc25626b0c85cfaad6b66aa60ac38a92a5969
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-20 04:24:53 +0000

    1010

--- trunk/protected/views/site/lastUpload.php
+++ trunk/protected/views/site/lastUpload.php
@@ -1,42 +1,28 @@
-<?php
-Helper::register("jquery.touchcarousel-1.0.min.js");
-Helper::register("jquery.ba-hashchange.min.js");
-Helper::register("dimsemenov-preview-pack.css");
-
-?>
 <div id="last_upload">Last Upload</div>
 
 <script type="text/javascript">
     jQuery(function($) {
-        carouselInstance = $("#carousel-image-and-text").touchCarousel({
-            pagingNav: false,
-            snapToItems: false,
-            itemsPerMove: 4,
-            scrollToLast: false,
-            loopItems: true,
-            scrollbar: false
-        }).data('touchCarousel');
+
     });
 </script>
-<div id="carousel-image-and-text" class="touchcarousel grey-blue">
-    <ul class="touchcarousel-container">
-        <?php
-        Helper::YiiImport("GetController");
-        $last = GetController::last_upload(10);
-        foreach ( $last as $key => $value ) {
-            $thumb = str_replace("photo/","",$value['link']);
-            $l = Yii::app()->baseUrl."/thumb/thumb230_".$thumb;
-            $i = $value['id'];
 
-            echo <<<HTML
-            <li class="touchcarousel-item">
-            <a class="item-block" href="/?p={$i}">
-                <img src="{$l}" />
+<?php
+/*
+Helper::YiiImport("GetController");
+$last = GetController::last_upload(10);
+foreach ( $last as $key => $value ) {
+    $thumb = str_replace("photo/","",$value['link']);
+    $l = Yii::app()->baseUrl."/thumb/thumb230_".$thumb;
+    $i = $value['id'];
 
-            </a>
-        </li>
+    echo <<<HTML
+    <li class="touchcarousel-item">
+    <a class="item-block" href="/?p={$i}">
+        <img src="{$l}" />
+
+    </a>
+</li>
 HTML;
-        }
-        ?>
-    </ul>
-</div>
+}
+*/
+?>
