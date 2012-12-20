e4afbcc51439c11121bced3ab317d61060bc6029
aommiez <aommiez@gmail.com>
2012-12-20 06:06:41 +0000

    1010

--- protected/views/site/lastUpload.php
+++ protected/views/site/lastUpload.php
@@ -1,28 +1,61 @@
-<?php
-Helper::register("inflickity.js");
-?>
 <div id="last_upload">Last Upload</div>
 <style>
+    .cats {
+        list-style: none;
+        width: 2400px;
+        margin: 0;
+        padding: 0;
+    }
+
+    .cats li {
+        float: left;
+        width: 240px;
+        height: 200px;
+        overflow: hidden;
+    }
+
+    img {
+        display: block;
+    }
+
     .slider {
         width: 100%;
         height: 240px;
         overflow: hidden;
     }
+
+    .cats li:nth-child(5n+0) { background: black; }
+    .cats li:nth-child(5n+1) { background: blue; }
+    .cats li:nth-child(5n+2) { background: green; }
+    .cats li:nth-child(5n+3) { background: orange; }
+    .cats li:nth-child(5n+4) { background: red; }
+
+    .inflickity-clone li {
+        /*  border-radius: 40px;*/
+    }
 </style>
 <script type="text/javascript">
+
     var init = function() {
 
         var slider1 = document.getElementById('slider1');
+        var slider2 = document.getElementById('slider2');
+        var cats = document.getElementById('cats');
 
         var myFlick = new Inflickity( slider1 );
 
+        var myDutchFlick = new Inflickity( slider2, {
+            offsetAngle: -Math.PI / 3
+        })
 
         window.myFlick = myFlick
 
     };
 
     window.addEventListener( 'DOMContentLoaded', init, false);
+
 </script>
+
 <div id="slider1" class="slider">
     <ul class="cats">
         <li><img src="http://placekitten.com/240/200/" /></li>
@@ -37,6 +70,26 @@ Helper::register("inflickity.js");
         <li><img src="http://placekitten.com/249/200/" /></li>
     </ul>
 </div>
+
+<div id="slider2" class="slider">
+    <ul class="cats">
+        <li><img src="http://placekitten.com/240/200/" /></li>
+        <li><img src="http://placekitten.com/241/200/" /></li>
+        <li><img src="http://placekitten.com/242/200/" /></li>
+        <li><img src="http://placekitten.com/243/200/" /></li>
+        <li><img src="http://placekitten.com/244/200/" /></li>
+        <li><img src="http://placekitten.com/245/200/" /></li>
+        <li><img src="http://placekitten.com/246/200/" /></li>
+        <li><img src="http://placekitten.com/247/200/" /></li>
+        <li><img src="http://placekitten.com/248/200/" /></li>
+        <li><img src="http://placekitten.com/249/200/" /></li>
+    </ul>
+</div>
+
+
+
+
+
 <?php
 /*
 Helper::YiiImport("GetController");
