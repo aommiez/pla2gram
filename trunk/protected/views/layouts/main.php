ec6f22b661c9c9eaf83d14cc26f54624f634ee92
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-20 04:19:55 +0000

    1010

--- trunk/protected/views/layouts/main.php
+++ trunk/protected/views/layouts/main.php
@@ -40,12 +40,13 @@ if ( isset($_GET['code'])) {
                 countImg++;
                 if(countImg==imgLength){ // เมื่อโหลดรูปทั้งหมดแล้วปิดตัว loading
                     $("#lightbox").hide();
-                    $("html, body").css("overflow"," ");
+                    $("html, body").css("overflow","none");
                 }
             });
             // เมื่อเกิดข้อผิดพลาดในการโหลดให้ปิด loading เลย
             $(this).error(function(){
                 $("#lightbox").hide();
+                $("html, body").css("overflow","none");
             });
 
         });
