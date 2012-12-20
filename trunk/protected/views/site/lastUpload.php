3e152a18b977cb99fead60d777f67a128f20dd68
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-20 06:02:12 +0000

    1010

--- trunk/protected/views/site/lastUpload.php
+++ trunk/protected/views/site/lastUpload.php
@@ -1,11 +1,42 @@
+<?php
+Helper::register("inflickity.js");
+?>
 <div id="last_upload">Last Upload</div>
-
+<style>
+    .slider {
+        width: 100%;
+        height: 240px;
+        overflow: hidden;
+    }
+</style>
 <script type="text/javascript">
-    jQuery(function($) {
+    var init = function() {
 
-    });
+        var slider1 = document.getElementById('slider1');
+
+        var myFlick = new Inflickity( slider1 );
+
+
+        window.myFlick = myFlick
+
+    };
+
+    window.addEventListener( 'DOMContentLoaded', init, false);
 </script>
-
+<div id="slider1" class="slider">
+    <ul class="cats">
+        <li><img src="http://placekitten.com/240/200/" /></li>
+        <li><img src="http://placekitten.com/241/200/" /></li>
+        <li><img src="http://placekitten.com/242/200/" /></li>
+        <li><img src="http://placekitten.com/243/200/" /></li>
+        <li><img src="http://placekitten.com/244/200/" /></li>
+        <li><img src="http://placekitten.com/245/200/" /></li>
+        <li><img src="http://placekitten.com/246/200/" /></li>
+        <li><img src="http://placekitten.com/247/200/" /></li>
+        <li><img src="http://placekitten.com/248/200/" /></li>
+        <li><img src="http://placekitten.com/249/200/" /></li>
+    </ul>
+</div>
 <?php
 /*
 Helper::YiiImport("GetController");
