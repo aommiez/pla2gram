3d9d0109ba1db368de1db15f3d5bb79f9051ca44
aommiez@gmail.com <aommiez@gmail.com@215ab714-d6ef-2afd-b69e-7c3305c170b7>
2012-12-20 08:27:37 +0000

    01

--- trunk/protected/views/site/lastUpload.php
+++ trunk/protected/views/site/lastUpload.php
@@ -72,13 +72,9 @@
 
         $(".aLast").mouseup(function(e){
             if ( clicker == false ) {
-                e.stopPropagation();
                 e.preventDefault();
-                // these two are older ways I like to add to maximize browser compat
-                e.returnValue = false;
-                e.cancelBubble = true;
-                return false;
             }
+            alert(clicker);
         });
 
 
