b519a785967aaff85cc4eabfcc33d033ba6cb153
aommiez <aommiez@gmail.com>
2012-12-24 06:24:41 +0000

    00101

--- protected/views/site/lastUpload.php
+++ protected/views/site/lastUpload.php
@@ -67,8 +67,6 @@
         });
 
 
-
-
         $(document).mouseup(function(e){
             $(document).unbind('mousemove');
             if ( clicker == false ) {
@@ -77,6 +75,42 @@
                 });
             } else {
                 $(".aLast").unbind('click');
+            }
+        });
+
+        $(".aLast").bind('touchstart',function(){
+            e.preventDefault();
+            clicker = true;
+            hereVal = e.pageX;
+            scVal =  $('.slider').scrollLeft();
+            mms = true;
+            $(document).bind('touchmove',function(e){
+                clicker = false;
+                if ( mms != false ) {
+                    var newVal = e.pageX;
+                    var LR = 0;
+                    if ( hereVal > newVal ) {
+                        LR = hereVal - newVal;
+                        $('.slider').scrollLeft(scVal+LR);
+                    } else if ( newVal > hereVal ) {
+                        LR = newVal - hereVal;
+                        $('.slider').scrollLeft(scVal-LR);
+                    }
+                    $('#theater').html(e.pageX +', '+ e.pageY + ', ' + LR + ',' + scVal);
+
+                }
+            });
+        });
+
+        $(document).bind('touchmove',function(){
+            clicker = false;
+        });
+
+        $(document).bind('touchend',function(e){
+            $(document).unbind('touchmove');
+            if ( clicker != false) {
+                var url = $(this).attr("href");
+                window.location.href = "http://www.pla2gram.com/"+url;
             }
         });
 
