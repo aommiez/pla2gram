f83f3488c38036f5def91114e5dc2861c62502f3
aommiez <aommiez@gmail.com>
2012-12-25 09:20:24 +0000

    00101

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -54,6 +54,17 @@ if ( Yii::app()->facebook->getUser() == 0) {
             document.location.href= "http://www.pla2gram.com<?php echo Yii::app()->createUrl("site/album"); ?>";
         });
 
+        $("#capPhoto").click(function(){
+            var textInBox = $(this).val();
+            if ( textInBox != "เพิ่มคำอธิบายรูป...." ) {
+                return true;
+            } else {
+                $(this).val("");
+                $(this).css("color","#333");
+                return true
+            }
+        });
+
     });
 
     function showTxt(){
